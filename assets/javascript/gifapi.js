d3ec663059e8d8f73afddc8c87fa5c2d1cf0676b
reb2039607 <reb.aguayo@gmail.com>
2019-06-26 22:58:51 -0700

    animal buttons added

--- assets/javascript/gifapi.js
+++ assets/javascript/gifapi.js
@@ -1,1 +1,17 @@
-alert("Party Time! Excellent!")
+$(function(){
+    populateButtons(searchArray, "searchButton", "#buttonsArea");
+    console.log("Page Loaded");
+})
+
+let searchArray = ["Cat", "Hamster", "Dragon"];
+
+function populateButtons(searchArray, classToAdd, areaToAddTo){
+    $(areaToAddTo).empty();
+    for(let i = 0; i < searchArray.length; i++){
+        let a = $("<button>");
+        a.addClass(classToAdd);
+        a.attr("data-type", searchArray[i]);
+        a.text(searchArray[i]);
+        $(areaToAddTo).append(a);
+    }
+}
